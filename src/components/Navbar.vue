8427217d0a7190eae7b66a9c59e16c9d59cd1fa5
Ana Clara <anaclaranoronha.m@gmail.com>
2026-03-30 17:27:40 -0400

    feat: add Navbar component and update HomeView

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+</style>
